22b0756880c12e7549b66e2d9912d747ab927cec
Vandana <vandanasharmajewar007@gmail.com>
2025-02-17 09:22:44 +0530

    done

---
+++ reverse.java
@@ -0,0 +1,60 @@
+public class reverse {
+    class Node{
+        int data;
+        Node next;
+        public Node (int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void reversell(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+
+        }
+        head=prev;
+    } public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    
+    public static void main(String[] args) {
+        reverse list = new reverse();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        
+        System.out.println("Original Linked List:");
+        list.display();
+        
+        list.reversell();
+        
+        System.out.println("Reversed Linked List:");
+        list.display();
+    }
+}
+
